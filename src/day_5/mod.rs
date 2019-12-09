384df47a4d88b5e4690eb332b2329f1bf976e6b6
Marc Jamot <marcjamot@gmail.com>
2019-12-09 12:55:12 +0100

    Add missing updates for day 5

--- src/day_5/mod.rs
+++ src/day_5/mod.rs
@@ -30,43 +30,71 @@ fn execute(memory: &mut Vec<i32>) -> i8 {
             memory[p], opcode, am, bm, rm
         );
         match opcode {
+            // Addition
             1 => {
                 let a = get_value(memory, am, memory[p + 1]);
                 let b = get_value(memory, bm, memory[p + 2]);
-                let r = get_value(memory, 1, memory[p + 3]) as usize;
+                let r = memory[p + 3] as usize;
                 memory[r] = a + b;
-                println!(" {} = {} + {}", r, a, b);
                 p += 4;
             }
+            // Multiplication
             2 => {
                 let a = get_value(memory, am, memory[p + 1]);
                 let b = get_value(memory, bm, memory[p + 2]);
-                let r = get_value(memory, 1, memory[p + 3]) as usize;
+                let r = memory[p + 3] as usize;
                 memory[r] = a * b;
-                println!(" {} = {} * {}", r, a, b);
                 p += 4;
             }
+            // Get input
             3 => {
-                let a = get_value(memory, 1, memory[p + 1]) as usize;
-                let mut buffer = String::new();
-                stdin()
-                    .read_line(&mut buffer)
-                    .expect("Input not a correct string.");
-                if let Some('\n') = buffer.chars().next_back() {
-                    buffer.pop();
-                }
-                if let Some('\r') = buffer.chars().next_back() {
-                    buffer.pop();
-                }
-                let v = buffer.parse().unwrap();
-                memory[a] = v;
-                println!(" {} = {}", a, v);
+                let r = memory[p + 1] as usize;
+                let v = get_input();
+                memory[r] = v;
+                println!(" {} = {}", r, v);
                 p += 2;
             }
+            // Print
             4 => {
-                let a = get_value(memory, am, memory[p + 1]) as usize;
-                println!("{}", a);
+                let a = get_value(memory, am, memory[p + 1]);
+                println!("INFO: {}", a);
                 p += 2;
+            }
+            // Jump if true
+            5 => {
+                let a = get_value(memory, am, memory[p + 1]);
+                let b = get_value(memory, bm, memory[p + 2]) as usize;
+                if a != 0 {
+                    p = b;
+                } else {
+                    p += 3;
+                }
+            }
+            // Jump if false
+            6 => {
+                let a = get_value(memory, am, memory[p + 1]);
+                let b = get_value(memory, bm, memory[p + 2]) as usize;
+                if a == 0 {
+                    p = b;
+                } else {
+                    p += 3;
+                }
+            }
+            // Less than
+            7 => {
+                let a = get_value(memory, am, memory[p + 1]);
+                let b = get_value(memory, bm, memory[p + 2]);
+                let r = memory[p + 3] as usize;
+                memory[r] = if a < b { 1 } else { 0 };
+                p += 4;
+            }
+            // Equals
+            8 => {
+                let a = get_value(memory, am, memory[p + 1]);
+                let b = get_value(memory, bm, memory[p + 2]);
+                let r = memory[p + 3] as usize;
+                memory[r] = if a == b { 1 } else { 0 };
+                p += 4;
             }
             99 => {
                 return 0;
@@ -90,7 +118,7 @@ fn parse_instruction(mut instruction: i32) -> (i32, i32, i32, i32) {
 }
 
 fn get_value(memory: &Vec<i32>, mode: i32, parameter: i32) -> i32 {
-    let mut v: i32;
+    let v: i32;
     if mode == 1 {
         v = parameter;
     } else {
@@ -98,4 +126,19 @@ fn get_value(memory: &Vec<i32>, mode: i32, parameter: i32) -> i32 {
     }
     println!("  get_value(memory, {}, {}) -> {}", mode, parameter, v);
     return v;
+}
+
+fn get_input() -> i32 {
+    print!("INPUT: ");
+    let mut buffer = String::new();
+    stdin()
+        .read_line(&mut buffer)
+        .expect("Input not a correct string.");
+    if let Some('\n') = buffer.chars().next_back() {
+        buffer.pop();
+    }
+    if let Some('\r') = buffer.chars().next_back() {
+        buffer.pop();
+    }
+    return buffer.parse().unwrap();
 }
