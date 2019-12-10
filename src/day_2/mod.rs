b4df22b5a7a9b769bda2a43336e8cbee03716157
Marc Jamot <marcjamot@gmail.com>
2019-12-10 19:39:37 +0100

    Use computer in day 2

--- src/day_2/mod.rs
+++ src/day_2/mod.rs
@@ -1,77 +1,50 @@
+use crate::icc::icc::{new, Computer};
 use std::fs;
 
 pub fn run(input: &str) {
     let inputs = load_inputs(input);
-    part_1(&inputs);
-    part_2(&inputs);
-}
-
-fn load_inputs(file_name: &str) -> Vec<usize> {
-    let content = fs::read_to_string(file_name).expect("Could not read file");
-    return content
-        .split(",")
-        .map(|x| x.parse::<usize>().unwrap())
-        .collect();
-}
-
-fn part_1(inputs: &Vec<usize>) {
-    let mut instructions = inputs.clone();
-    instructions[1] = 12;
-    instructions[2] = 2;
-    execute(&mut instructions);
+    let ans_1 = part_1(&inputs);
+    println!("Part 1: {}", ans_1);
+    let (noun, verb) = part_2(&inputs);
     println!(
-        "Part 1: [{}]",
-        instructions
-            .iter()
-            .map(|x| x.to_string())
-            .collect::<Vec<String>>()
-            .join(",")
+        "Part 2: noun {} and verb {} -> {}",
+        noun,
+        verb,
+        noun * 100 + verb
     );
 }
 
-fn part_2(inputs: &Vec<usize>) {
+fn load_inputs(file_name: &str) -> Vec<i64> {
+    let content = fs::read_to_string(file_name).expect("Could not read file");
+    return content
+        .split(",")
+        .map(|x| x.parse::<i64>().unwrap())
+        .collect();
+}
+
+fn part_1(inputs: &Vec<i64>) -> i64 {
+    let mut instructions = inputs.clone();
+    instructions[1] = 12;
+    instructions[2] = 2;
+    let mut computer = new(instructions.len(), &instructions);
+    assert_eq!(computer.execute(), 0);
+    return computer.get_memory_at(0);
+}
+
+fn part_2(inputs: &Vec<i64>) -> (i64, i64) {
     for noun in 0..100 {
         for verb in 0..100 {
             let mut instructions = inputs.clone();
             instructions[1] = noun;
             instructions[2] = verb;
-            if !execute(&mut instructions) {
+            let mut computer = new(instructions.len(), &instructions);
+            if computer.execute() != 0 {
                 continue;
             }
-            if instructions[0] == 19690720 {
-                println!(
-                    "Part 2: Noun [{}] Verb [{}] -> [{}]",
-                    noun,
-                    verb,
-                    instructions
-                        .iter()
-                        .map(|x| x.to_string())
-                        .collect::<Vec<String>>()
-                        .join(",")
-                );
-                return;
+            if computer.get_memory_at(0) == 19690720 {
+                return (noun, verb);
             }
         }
     }
-    println!("No answer found for part 2 :(");
-}
-
-fn execute(instructions: &mut Vec<usize>) -> bool {
-    let mut pointer = 0;
-    while instructions[pointer] != 99 {
-        let a = instructions[pointer + 1];
-        let b = instructions[pointer + 2];
-        let r = instructions[pointer + 3];
-        match instructions[pointer] {
-            1 => {
-                instructions[r] = instructions[a] + instructions[b];
-            }
-            2 => {
-                instructions[r] = instructions[a] * instructions[b];
-            }
-            _ => return false,
-        }
-        pointer += 4;
-    }
-    return true;
+    panic!("No answer found for part 2 :(");
 }
